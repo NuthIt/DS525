b80c855f43c89313c7c344cd8a18da53aa295af2
Nuttapol Ittinirundorn <maxmisumaki@hotmail.com>
2022-11-05 11:47:55 +0000

    gitpod_update5

--- 06-analytics-engineering/jaffle/models/my_simple_model2.sql
+++ 06-analytics-engineering/jaffle/models/my_simple_model2.sql
@@ -1,4 +1,4 @@
 select
-    {{divide_by_hundred(1)}} as a
-    , {{divide_by_hundred(89)}} as b
-    , {{divide_by_hundred(2)}} as c
+    {{ divide_by_hundred(1) }} as a
+    , {{ divide_by_hundred(89) }} as b
+    , {{ divide_by_hundred(20) }} as c
