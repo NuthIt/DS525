f10233527e075d8a60ade80e96d6478e202fc099
Nuttapol Ittinirundorn <maxmisumaki@hotmail.com>
2022-11-05 11:17:23 +0000

    gitpod_test3

--- 06-analytics-engineering/jaffle/models/completed_orders.sql
+++ 06-analytics-engineering/jaffle/models/completed_orders.sql
@@ -1,1 +1,12 @@
-SELECT * FROM jaffle_shop_orders WHERE STATUS = 'completed'
+select
+    o.id
+    , o.order_date
+    , o.status
+    , c.first_name
+    , c.last_name
+
+from jaffle_shop_orders as o
+join jaffle_shop_customers as c
+on
+    o.user_id = c.id
+where status = 'completed'
